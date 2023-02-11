367253877c300decd950bb4ead6a0bdef8d63f48
ashwani <55181339+Ashwani-Salaria@users.noreply.github.com>
2023-02-11 23:00:35 +0530

    Update app_setting.dart

--- lib/Config/app_setting.dart
+++ lib/Config/app_setting.dart
@@ -1,27 +1,6 @@
 final Map<String, String> appSettings = {
-  // ANT MEDIA SERVER URL CURRENTLY NOT IN USE //
-  // "ant_socket_url": "https://api.bobotracker.com/WebRTCAppEE/websocket",
 
   // LIVE app.knowex SERVER URL //
-  "api_url": "https://api.boomceleb.com/",
-  // "socket_url": "wss://api.boomceleb.com/",
-  "socket_url": "https://api.boomceleb.com/",
-
-  "callListener": "call",
-  "liveListener": "live",
-
-// STAGING Stagingapp.knowex SERVER URL //
-
-  // "currency_id": "f45654654456g",
-
-  // PAYPAL API URL FOR SANDBOX MODE, COMMENT THIS FOR LIVE BUILD //
-  // "paypal_url": "https://api.sandbox.paypal.com",
-  // PAYPAL API URL FOR PRODUCTION MODE, PLEASE USE THIS API URL FOR LIVE BUILD //
-  // "paypal_url": "https://api.paypal.com",
-  // "paypal_client_id":
-  //     'AcXcC7cU7kNgcNBIZWik6YtZIu3f8rvP83kjk5FBOAaMKAcmVKzgccggh5oNvR9k9Wgpli1IjrbkEhrc',
-  // "paypal_secret":
-  //     'EGXfI-HZaNBlnk-XfRFHAbxe5W9LNWAipPMdWx4aabU07aHOYDVvZ755TM9L19g3kJVSX4k_Ywlb_Ady',
-  // AGORA APP ID //
-  // "agora_app_id": '0fda789a4bad40da8d108fd2ece78a86',
+  "api_url": "",
+ 
 };
